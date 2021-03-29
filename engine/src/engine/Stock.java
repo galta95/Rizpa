a4769be1b78b12fb092a24ee33a2df4b2c6cbae9
Gal <galtaebi@gmail.com>
2021-03-29 15:38:44 +0300

    feat(menu): create menu number 2

--- engine/src/engine/Stock.java
+++ engine/src/engine/Stock.java
@@ -2,15 +2,24 @@ package engine;
 
 import dataManager.jaxb.generated.RseStock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
     private String symbol;
     private String companyName;
     private int price;
+    private List<Deal> deals;
+    private int dealsCount;
+    private int cycle;
 
     public Stock(String symbol, String companyName, int price) {
         this.symbol = symbol;
         this.companyName = companyName;
         this.price = price;
+        this.deals = new ArrayList<Deal>();
+        this.dealsCount = 0;
+        this.cycle = 0;
     }
 
     public Stock(RseStock stock) {
@@ -30,6 +39,12 @@ public class Stock {
     public String getSymbol() {
         return symbol;
     }
+
+    public List<Deal> getDeals() { return deals; }
+
+    public int getDealsCount() { return dealsCount; }
+
+    public int getCycle() { return cycle; }
 
     public void setPrice(int price) {
         this.price = price;
