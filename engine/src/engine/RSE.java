9dfbf67e7e493ab6e9ca40e7fad0008f473ecca9
galta95 <48960890+galta95@users.noreply.github.com>
2021-03-31 10:38:05 +0300

    feat(menu): create menu number 2 (#9)
    * feat(menu): create menu number 2

--- engine/src/engine/RSE.java
+++ engine/src/engine/RSE.java
@@ -3,11 +3,16 @@ package engine;
 import dataManager.jaxb.generated.RizpaStockExchangeDescriptor;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RSE {
     private Stocks stocks;
 
     public RSE(RizpaStockExchangeDescriptor rsed) {
         this.stocks = new Stocks(rsed.getRseStocks());
+    }
+
+    public Stocks getStocks() {
+        return this.stocks;
     }
 }
--- ui/src/menu/Menu.java
+++ ui/src/menu/Menu.java
@@ -1,20 +1,45 @@
 package menu;
 
-import engine.Handler;
-import engine.RSE;
+import engine.*;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Menu {
     public static void main(String[] args) {
         RSE rse;
-        int userChoise;
-
-        System.out.println("1. Load XML");
-        Scanner s = new Scanner(System.in);
-        userChoise = s.nextInt();
-
         rse = Handler.startApp("engine/src/resources/ex1-small.xml");
-        System.out.println("Bye");
+        choiseTwo(rse.getStocks());
+    }
+
+    public static void choiseTwo(Stocks stocks) {
+        Map<String, Stock> hashStocks = stocks.getStocks();
+        System.out.println("Stocks list: ");
+
+        hashStocks.forEach((symbol, stock) -> {
+            printStockDetails(stock);
+            System.out.println();
+        });
+    }
+
+    public static void printStockDetails(Stock stock) {
+        System.out.println(
+                "\t" + "Stock symbol: " + stock.getSymbol() + "\n" +
+                "\t" + "Stock company name: " + stock.getCompanyName() + "\n" +
+                "\t" + "Current price: " + stock.getPrice() + "\n" +
+                "\t" + "Deals until now: " + stock.getDealsCount() + "\n" +
+                "\t" + "Stock cycle: " + stock.getCycle()
+        );
+    }
+
+    public static void printStockDeals(Stock stock) {
+        List<Deal> deals = stock.getDeals();
+        for (Deal deal : deals) {
+            System.out.println(
+                    "\t" + "Date: " + deal.getDate() +
+                    "\t" + "Number of shares: " + deal.getNumOfShares() +
+                    "\t" + "Sold price: " + deal.getSoldPrice() +
+                    "\t" + "Deal value: " + deal.getDealValue() + "\n"
+            );
+        }
     }
 }
