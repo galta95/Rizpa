3d4a42ae3956c513aa66277f557f4b2f80b39fcb
Gal <galtaebi@gmail.com>
2021-07-05 21:28:06 +0300

    feat(feed): init feed

---
+++ ui/src/servlets/UsersServlets.java
@@ -0,0 +1,27 @@
+package servlets;
+import com.google.gson.Gson;
+import engine.dto.DTOUsers;
+import engine.stockMarket.StockMarketApi;
+import utils.ServletUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UsersServlets extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json");
+        StockMarketApi stockMarketApi = ServletUtils.getStockMarketApi(getServletContext());
+
+        DTOUsers users = stockMarketApi.getAllUsers();
+
+        Gson gson = new Gson();
+        String jsonResponse;
+        jsonResponse = gson.toJson(users);
+        res.getWriter().print(jsonResponse);
+    }
+}
---
+++ ui/web/pages/feed/feed.js
@@ -0,0 +1,28 @@
+let localUsers = {};
+
+const createListItem = (username) => {
+    let listItem = document.createElement('li');
+    listItem.className = 'list-group-item'
+    listItem.textContent = username;
+    return listItem;
+}
+
+const getAllUsers = async () => {
+    const myList = document.querySelector('#users-list');
+
+    await fetch(`http://localhost:8080/ui_Web_exploded/users`)
+        .then(res => res.json())
+        .then(data => {
+            data.users.forEach(user => {
+                if (localUsers[user.name] === undefined) {
+                    localUsers[user.name] = user.name;
+                    const listItem = createListItem(user.name);
+                    myList.appendChild(listItem);
+                }
+            })
+        }).catch(e => console.log(e))
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    setInterval(getAllUsers, 2000);
+});
