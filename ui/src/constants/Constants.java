9e4d5f5496ae711ddd4ba38586856452923d8345
Yuval <artziyuval@gmail.com>
2021-07-11 22:10:33 +0300

    fear(trade): possible to trade stocks

--- ui/src/constants/Constants.java
+++ ui/src/constants/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String BAD_REQUEST = "bad request";
     public static final String CONFLICT = "conflict";
     public static final String CREATED = "created";
+    public static final String OK = "ok";
 }
